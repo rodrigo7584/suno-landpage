ae9c831861bb7840e4fe10b4c3a131841b93b564
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2024-06-23 22:31:04 -0300

    23/06/2024

--- js/script.js
+++ js/script.js
@@ -36,10 +36,9 @@ function sendData(table) {
   const speed = chosenTable.querySelector('.speed').innerText
   const price = chosenTable.querySelector('.price').innerText
   let time = chosenTable.querySelector('.time')
-  const btn = chosenTable.querySelector('.btn-switch.active')
-  let adicional = ''
-  if (btn) {
-    adicional = btn
+  let adicional = chosenTable.querySelector('.btn-switch.active')
+  if (adicional) {
+    adicional = adicional
       .closest('.adicional-box')
       .querySelector('.description').innerText
   }
@@ -47,38 +46,128 @@ function sendData(table) {
     time = time.innerText
   }
 
-  console.log(
-    `velocidade:${speed}|preço:${price}|tempo:${time}|tem adicional:${adicional}`
-  )
+  // console.log(
+  //   `velocidade:${speed}|preço:${price}|tempo:${time}|adicional:${adicional}`
+  // )
 
-  // const data = {
-  //   speed: speed
-  // }
-
-  // const queryString = new URLSearchParams(data).toString()
-  // window.location.href = 'compra.html?' + queryString
+  const data = {
+    price: price,
+    speed: speed,
+    time: time,
+    adicional: adicional
+  }
+  const queryString = new URLSearchParams(data).toString()
+  window.location.href = 'compra.html?' + queryString
 }
 
 // Função para obter parâmetros da query string
 function getQueryParams() {
   const params = new URLSearchParams(window.location.search)
   return {
-    speed: params.get('speed')
+    price: params.get('price'),
+    speed: params.get('speed'),
+    time: params.get('time'),
+    adicional: params.get('adicional')
   }
 }
 
 // Função para exibir dados na Página 2
 function displayData() {
   const data = getQueryParams()
-  const dataDisplay = document.getElementById('dataDisplay')
+  const speed = document.querySelector('.menu .speed')
+  const price = document.querySelector('.menu .price')
+  const adicional = document.querySelector('.menu .adicional')
+  const time = document.querySelector('.menu .time')
+  const velocidade = document.querySelector('.form .velocidade')
+  if (velocidade) {
+    velocidade.value = data.speed
+  }
 
-  if (dataDisplay) {
-    dataDisplay.innerHTML = `
-          <p>speed: ${data.speed}</p>
-      `
+  if (speed) {
+    speed.innerText = data.speed
+  }
+
+  if (price) {
+    price.innerText = data.price
+  }
+
+  if (adicional) {
+    if (data.adicional !== 'null') {
+      adicional.innerText = data.adicional
+    } else {
+      adicional.innerText = ''
+    }
+  }
+
+  if (time) {
+    if (data.time !== 'null') {
+      time.innerText = data.time
+    } else {
+      time.innerText = ''
+    }
   }
 }
 // Verifica se está na Página 2 e exibe os dados
 if (window.location.pathname.endsWith('compra.html')) {
   window.onload = displayData
+}
+//form
+let currentStep = 0
+showStep(currentStep)
+
+function showStep(n) {
+  const steps = document.getElementsByClassName('step')
+  steps[n].classList.add('active')
+  if (n == 0) {
+    document.getElementById('prevBtn').style.display = 'none'
+  } else {
+    document.getElementById('prevBtn').style.display = 'inline'
+  }
+  if (n == steps.length - 1) {
+    document.getElementById('nextBtn').innerText = 'Enviar'
+    document.getElementById('nextBtn').setAttribute('type', 'submit')
+  } else {
+    document.getElementById('nextBtn').innerText = 'Continuar'
+    document.getElementById('nextBtn').setAttribute('type', 'button')
+  }
+}
+
+function nextPrev(n) {
+  const steps = document.getElementsByClassName('step')
+  if (n == 1 && !validateForm()) return false
+  steps[currentStep].classList.remove('active')
+  currentStep += n
+  if (currentStep >= steps.length) {
+    document.getElementById('multiStepForm').submit()
+    return false
+  }
+  showStep(currentStep)
+}
+
+function validateForm() {
+  const steps = document.getElementsByClassName('step')
+  const inputs = steps[currentStep].querySelectorAll('input[required]')
+  let valid = true
+
+  inputs.forEach(input => {
+    if (input.value.trim() === '') {
+      input.classList.add('invalid')
+      valid = false
+    } else {
+      input.classList.remove('invalid')
+    }
+  })
+
+  return valid
+}
+
+function setActivePayday(label) {
+  const allLabels = document.querySelectorAll('.payday-option')
+  allLabels.forEach(lbl => lbl.classList.remove('active'))
+  label.classList.add('active')
+}
+function setActivePayType(label) {
+  const allLabels = document.querySelectorAll('.paytype-option')
+  allLabels.forEach(lbl => lbl.classList.remove('active'))
+  label.classList.add('active')
 }
